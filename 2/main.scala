acb1276d56e99529dd47ae5d3d8cbe13695e078a
timrcoulson <tim.r.coulson@googlemail.com>
2015-05-06 23:59:49 +0100

    add solution for 2

---
+++ 2/main.scala
@@ -0,0 +1,23 @@
+import scala.collection.mutable.ArrayBuffer
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println(fib(1, 4000000).filter(x => isMultiple(x,2)).reduceLeft(_+_))
+  }
+
+  def fib(start: Int, limit: Int) : ArrayBuffer[Int] = {
+    var arr = ArrayBuffer(start, start)
+    var last = start
+    while ( arr.last < limit ) {
+      arr += arr(arr.length - 1) + arr(arr.length - 2) 
+    }
+    return arr
+  }
+ 
+  def isMultiple(multiple: Int, factor: Int) : Boolean = {
+    if ((multiple % factor) == 0) {
+      return true
+    }
+    return false
+  }
+}
