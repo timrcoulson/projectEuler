b784a21351c00d443d496e46cc5e23ab3752adf6
timrcoulson <tim.r.coulson@googlemail.com>
2015-05-06 23:30:41 +0100

    add solution to problem 1

---
+++ 1/main.scala
@@ -0,0 +1,19 @@
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+    val multiples = getMultiples(999, Array(3,5))
+    println(multiples.deep.mkString("\n"))
+    println("Sum = " + multiples.reduceLeft(_+_))
+  }
+
+  def getMultiples(x: Int, factors: Array[Int]): Array[Int] = {
+    return (1 to x).filter(y => factors.exists(factor => isMultiple(y, factor))).toArray
+  }
+ 
+  def isMultiple(multiple: Int, factor: Int) : Boolean = {
+    if ((multiple % factor) == 0) {
+      return true
+    }
+    return false
+  }
+}
